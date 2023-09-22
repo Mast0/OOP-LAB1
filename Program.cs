aed7572c50dbd9c3c3b4d85f759f62cb4f88784c
Bogda <bogdanremuga@gmail.com>
2023-09-23 00:20:42 +0300

    Add class User

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GameClasses;
+
+namespace MainClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Mast");
+            Console.WriteLine(user.GetUserName());
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClasses
+{
+    class User
+    {
+        private string UserName { get; }
+        private int CurrentRating { get; set; }
+        private int GameCount { get; set; }
+
+        public User(string userName) {
+            UserName = userName;
+            CurrentRating = 0;
+            GameCount = 0;
+        }
+
+        public string GetUserName()
+        {
+            return UserName;
+        }
+
+        public int GetCurentRaiting()
+        {
+            return CurrentRating;
+        }
+
+        public int GetGameCount()
+        {
+            return GameCount;
+        }
+
+        public void WinGame()
+        {
+            CurrentRating++;
+            GameCount++;
+        }
+
+        public void LoseGame()
+        {
+            if (CurrentRating-- < 0)
+            {
+                throw new InvalidOperationException("The raiting cannot be negative");
+            }
+            CurrentRating--;
+            GameCount++;
+        }
+    }
+}
